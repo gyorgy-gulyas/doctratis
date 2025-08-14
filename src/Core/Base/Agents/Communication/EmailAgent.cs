509ce124b0ecb0684be7185cf0383c3b195dfc00
Gulyás György <gulyas.gyorgy@gmail.com>
2025-08-14 08:59:28 +0200

    WIP

---
+++ src/Core/Base/Agents/Communication/EmailAgent.cs
@@ -0,0 +1,19 @@
+﻿using ServiceKit.Net;
+using ServiceKit.Net.Communicators;
+
+namespace Core.Base.Agents.Communication
+{
+    public class EmailAgent
+    {
+        private readonly IEmailCommunicator _emailCommunicator;
+        public EmailAgent(IEmailCommunicator emailCommunicator)
+        {
+            _emailCommunicator = emailCommunicator;
+        }
+
+        public Task<Response> SendOTP(CallingContext ctx, string emailAddress, string otp)
+        {
+            return _emailCommunicator.SendEmail("one time otp for docratis", $"Docratis bejelentkeési kód: {otp}", [emailAddress]);
+        }
+    }
+}
---
+++ src/Core/Base/Agents/Communication/SmsAgent.cs
@@ -0,0 +1,19 @@
+﻿using ServiceKit.Net;
+using ServiceKit.Net.Communicators;
+
+namespace Core.Base.Service.Communication
+{
+    public class SmsAgent
+    {
+        private readonly ISmsCommunicator _smsCommunicator;
+        public SmsAgent(ISmsCommunicator smsCommunicator)
+        {
+            _smsCommunicator = smsCommunicator;
+        }
+
+        public Task<Response> SendOTP(CallingContext ctx, string phoneNumber, string otp)
+        {
+            return _smsCommunicator.SendSMS(phoneNumber, $"Docratis bejelentkeési kód: {otp}");
+        }
+    }
+}
